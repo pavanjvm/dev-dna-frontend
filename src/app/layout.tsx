a49f26797107a0b6457396339c676137b6411d74
pavan prasad <pavanprasad0605@gmail.com>
2025-08-08 06:46:10 +0000

    can we have a minimalistic font which vert classy and asethetic instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
-          rel="stylesheet"
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap" 
+          rel="stylesheet" 
         />
       </head>
       <body className="font-body antialiased">
